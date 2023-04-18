d307774fd06072d5600256e8387e3ae637159dba
lorenzoaniel <lorenzoaniel@gmail.com>
2023-04-17 19:16:22 -0600

    for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { readFileSync } from "fs";
+
+const pkg = JSON.parse(readFileSync(resolve(__dirname, "package.json"), "utf-8"));
+const homepage = pkg.homepage;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	base: process.env.NODE_ENV === "production" ? homepage : "/",
+	plugins: [react()],
+});
